f1e297a8f84e0c8f5c395e632c0e3e2e207c2f2d
Ankur Garg <ankurgarg101@gmail.com>
2018-11-21 20:09:29 -0600

    adds code to create the data split according to the specified configuration

--- NMT/preprocess.py
+++ NMT/preprocess.py
@@ -10,23 +10,28 @@
 
 import os
 import sys
+import argparse
+from os import path
 
 from src.logger import create_logger
 from src.data.dictionary import Dictionary
+from splitter import create_data_splits
 
+def parse_args(parser):
 
-if __name__ == '__main__':
+    parser.add_argument('--data-dir', dest='data_dir', type=str, required=True, help="Data directory of input data")
+    parser.add_argument('--out-dir', dest='out_dir', type=str, required=True, help="Output data directory path")
+    parser.add_argument('--name', type=str, required=True, help="Name of the configuration")
 
-    logger = create_logger(None)
+    parser.add_argument('--file-info', dest="file_info", type=str, required=True, help='File Info of the form (fp1, paired_pct1, upaired_pct1, fp2, paired_pct2, upaired_pct2)')
 
-    voc_path = sys.argv[1]
-    txt_path = sys.argv[2]
-    bin_path = sys.argv[2] + '.pth'
-    assert os.path.isfile(voc_path)
-    assert os.path.isfile(txt_path)
+    parser.add_argument('--dev', type=str, required=True, help="Name of Dev Dataset")
 
-    dico = Dictionary.read_vocab(voc_path)
-    logger.info("")
+    parser.add_argument('--test', type=str, required=True, help="Name of Test Dataset")
+
+    return vars(parser.parse_args())
+
+def create_binary(txt_path, bin_path, dico):
 
     data = Dictionary.index_data(txt_path, bin_path, dico)
     logger.info("%i words (%i unique) in %i sentences." % (
@@ -34,6 +39,7 @@ if __name__ == '__main__':
         len(data['dico']),
         len(data['positions'])
     ))
+
     if len(data['unk_words']) > 0:
         logger.info("%i unknown words (%i unique), covering %.2f%% of the data." % (
             sum(data['unk_words'].values()),
@@ -45,3 +51,45 @@ if __name__ == '__main__':
                 logger.info("%s: %i" % (w, c))
     else:
         logger.info("0 unknown word.")
+
+if __name__ == '__main__':
+
+    logger = create_logger(None)
+
+    parser = argparse.ArgumentParser()
+    params = parse_args(parser)
+
+    create_data_splits(params)
+    langs = ['x', 'y']
+    split = ['para', 'mono']
+
+    for lang in langs:
+        
+        voc_path = path.join(params['out_dir'], params['name'], 'data', 'vocab.{}'.format(lang))
+
+        dico = Dictionary.read_vocab(voc_path)
+        logger.info("")
+        assert os.path.isfile(voc_path)
+
+        for spl in split:
+            
+            txt_path = path.join(params['out_dir'], params['name'], 'data', '{}.{}'.format(spl, lang))
+            bin_path = txt_path + '.pth'
+            assert os.path.isfile(txt_path)       
+
+            create_binary(txt_path, bin_path, dico)
+
+
+        txt_path = path.join(params['data_dir'], '{}.{}'.format(params['dev'], lang))
+        bin_path = path.join(params['out_dir'], params['name'], 'data', '{}.{}'.format(params['dev'], lang))
+        
+        assert os.path.isfile(txt_path)
+
+        create_binary(txt_path, bin_path, dico)
+
+        txt_path = path.join(params['data_dir'], '{}.{}'.format(params['test'], lang))
+        bin_path = path.join(params['out_dir'], params['name'], 'data', '{}.{}'.format(params['test'], lang))
+        
+        assert os.path.isfile(txt_path)
+
+        create_binary(txt_path, bin_path, dico)
