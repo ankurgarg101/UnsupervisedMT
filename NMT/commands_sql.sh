b22b681032ec8e1550238625b56c6101d168bd5a
Ankur Garg <ankurgarg101@gmail.com>
2018-12-13 00:09:34 -0600

    adds scripts for running sql data

---
+++ NMT/commands_sql.sh
@@ -0,0 +1,2 @@
+bash gen_data.sh /home/marauder/ut/nlp_cs388/final_proj/text2sql-clean/ /home/marauder/ut/nlp_cs388/final_proj/ 30 30 0 0
+bash run_sql.sh /home/marauder/ut/nlp_cs388/final_proj/sql/ 30 30 0 0 32 100 1 1 1
